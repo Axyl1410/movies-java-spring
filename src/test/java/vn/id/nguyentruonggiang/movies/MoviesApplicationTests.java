c60efa6f513307a7e36f17ed5aae4f62f3e7e139
AlexNe <truonggiang190689@gmail.com>
2024-10-19 02:02:06 +0700

    first commit

---
+++ src/test/java/vn/id/nguyentruonggiang/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.id.nguyentruonggiang.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
