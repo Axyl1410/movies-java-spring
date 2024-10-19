87865fd5391cb75ada33ad453b7de9ef06304c0d
Alex <truonggiang190689@gmail.com>
2024-10-20 03:09:03 +0700

    Update axiosConfig.ts

--- frontend/src/api/axiosConfig.ts
+++ frontend/src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://movie.nguyentruonggiang.id.vn",
   timeout: 5000,
   headers: {
     "ngrok-skip-browser-warning": "true",
