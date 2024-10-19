279d24a7822d38d1f36a112b2bbb13842e55d269
AlexNe <truonggiang190689@gmail.com>
2024-10-19 18:20:23 +0700

    review controller

--- src/main/java/vn/id/nguyentruonggiang/movies/MovieController.java
+++ src/main/java/vn/id/nguyentruonggiang/movies/MovieController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,12 @@ public class MovieController {
   @GetMapping
   public ResponseEntity<List<Movie>> allMovies() {
     return new ResponseEntity<>(movieService.getAllMovies(), HttpStatus.OK);
+  }
+
+  @GetMapping("/{imdbid}")
+  public  ResponseEntity<Movie> getMovieById(@PathVariable String imdbid) {
+    return movieService.getMovieById(imdbid)
+      .map(movie -> new ResponseEntity<>(movie, HttpStatus.OK))
+      .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
 }
